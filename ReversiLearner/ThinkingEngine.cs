24b13216bb409e2bd1295ad397a2dbcd7b8c58e9
Kohei <atihsa.mihs.ne.ho.da@gmail.com>
2017-05-28 23:59:28 +0900

    不要なコメントの削除

--- ReversiLearner/ThinkingEngine.cs
+++ ReversiLearner/ThinkingEngine.cs
@@ -81,49 +81,7 @@ namespace ReversiLearner
                     best = min.Value;
                     return min.Key;
                 }
-
             });
-            //return await Task.Run(() =>
-            //{
-            //    legalMoves = board.SearchLegalMoves(player); //合法手
-            //    if (legalMoves.Count == 0)
-            //    {
-            //        throw new InvalidOperationException("合法手がありません");
-            //    }
-            //    else
-            //    {
-            //        var best = -999999;
-            //        var bestMove = default(ReversiMove);
-            //        foreach (var item in board.SearchLegalMoves(player))
-            //        {
-            //            var child = board.AddStone(item.Row, item.Col, player);
-            //            var eval = evaluator.Execute(child.BlackToMat(),child.WhiteToMat());
-            //            switch (player)
-            //            {
-            //                case StoneType.None:
-            //                    break;
-            //                case StoneType.Sente:
-            //                    if (best < eval)
-            //                    {
-            //                        best = eval;
-            //                        bestMove = item;
-            //                    }
-            //                    break;
-            //                case StoneType.Gote:
-            //                    if (best < -eval)
-            //                    {
-            //                        best = -eval;
-            //                        bestMove = item;
-            //                    }
-            //                    break;
-            //                default:
-            //                    break;
-            //            }
-            //        }
-            //        return bestMove;
-            //    }
-            //});
-
         }
 
         int best = 0;
